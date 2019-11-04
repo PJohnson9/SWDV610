eb5465df04b25edf7904772467e49a1b88d940f4
PJohnson9 <56849400+PJohnson9@users.noreply.github.com>
2019-11-04 11:57:53 -0800

    Update SWDV610_Week3_1_MinMax.py
    Updating header, adding comments

--- SWDV610_Week3_1_MinMax.py
+++ SWDV610_Week3_1_MinMax.py
@@ -1,19 +1,20 @@
-# Patrick Johnson        10/23/2019 #
+# Patrick Johnson         11/4/2019 #
 # SWDV 610 3W 19/FA2    Week 3 - #1 #
 #####################################
 
 def find_min_max(sequence):
     """ Finds the minimum and maximum values of a sequence recursively.
     Returns a list with the minimum value first and the maximum value second."""
-    if len(sequence) == 1:
+    
+    if len(sequence) == 1:   # 1 item in the sequence is both the min and the max
         return [sequence[0], sequence[0]]
-    elif len(sequence) == 2:
+    elif len(sequence) == 2: # 2 items => return in the right order
         if sequence[0] < sequence[1]:
             return sequence
         else:
             return [sequence[1], sequence[0]]
 
-    elif len(sequence) == 3:
+    elif len(sequence) == 3: # 3 items => compare to return min and max
         if sequence[0] > sequence[1]:
             sequence[0],sequence[1] = sequence[1],sequence[0]
         if sequence[1] > sequence[2]:
@@ -22,11 +23,11 @@ def find_min_max(sequence):
                 sequence[0] = sequence[1]      
         return [sequence[0],sequence[2]]
 
-    else:
+    else:                    # >3 items => Recursively check tail 
         return find_min_max([sequence[0]] + find_min_max(sequence[1:]))
 
 
-
+# A few tests to run:
 if __name__ == "__main__":
     print(find_min_max([10,9,8,7,6,5,4,3,2,1]))
     print(find_min_max([1,2,3,4,5,6,7,8,9,10]))
