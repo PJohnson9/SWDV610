16ca4f5379274374bd5f55521fdcd23aa9e69744
PJohnson9 <56849400+PJohnson9@users.noreply.github.com>
2019-11-04 11:49:45 -0800

    Create SWDV610_Week3_2_Reverse.py

---
+++ SWDV610_Week3_2_Reverse.py
@@ -0,0 +1,23 @@
+# Patrick Johnson         11/4/2019 #
+# SWDV 610 3W 19/FA2    Week 3 - #2 #
+#####################################
+
+def reverse(sequence):
+    """ Recursively reverses a list"""
+    if len(sequence) <= 1:
+        return sequence
+    
+    elif len(sequence) == 2:
+            return [sequence[1], sequence[0]]
+
+    else:
+        return reverse(sequence[1:]) + [sequence[0]]
+
+
+if __name__ == "__main__":
+    print(reverse([10,9,8,7,6,5,4,3,2,1]))
+    print(reverse([1,2,3,4,5,6,7,8,9,10]))
+    print(reverse([10, 2, 2, 2, 1, 2, 9, 7, 2, 6, -96,
+                        15, 44, 96, 25, 27, 27, 0, -85, 34]))
+    print(reverse(range(2)))
+    print(reverse([42]))
