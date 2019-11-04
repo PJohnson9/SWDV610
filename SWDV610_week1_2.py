20bef9420e79df0e62c0a7b683196c75504529fc
PJohnson9 <56849400+PJohnson9@users.noreply.github.com>
2019-11-04 12:00:03 -0800

    Create SWDV610_week1_2.py

---
+++ SWDV610_week1_2.py
@@ -0,0 +1,10 @@
+# Patrick Johnson        10/22/2019 #
+# SWDV 610 3W 19/FA2    Week 1 - #2 #
+#####################################
+ 
+def main():
+    """ Prints "I will never spam my friends again." to the console 100 times."""
+    for i in range(100):
+        print("I will never spam my friends again.")
+ 
+main()
