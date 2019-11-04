5937b5f84259fc5f1b922f6bb83b5e2c07ccde34
PJohnson9 <56849400+PJohnson9@users.noreply.github.com>
2019-11-04 12:00:26 -0800

    Create SWDV610_Week1_1.py

---
+++ SWDV610_Week1_1.py
@@ -0,0 +1,7 @@
+# Patrick Johnson        10/23/2019 #
+# SWDV 610 3W 19/FA2    Week 1 - #1 #
+#####################################
+ 
+def is_multiple(n,m):
+    """returns True if n is a multiple of m, False otherwise"""
+    return n % m == 0
